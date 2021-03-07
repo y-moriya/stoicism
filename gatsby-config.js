0de72f7ffacec32a1d045637dfd30c06f35176f4
Yu MORIYA <euro21st@gmail.com>
2021-03-07 18:13:18 +0900

    Fix google analytics settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,11 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "process.env.GOOGLE_ANALYTICS_ID",
+          process.env.GOOGLE_ANALYTICS_ID,
         ],
+        pluginConfig: {
+          head: true,
+        },
       },
     },
     {
